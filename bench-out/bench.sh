8e297f50b2fe1837bb7b8cf1c7ced513cd7173fb
meooow25 <soumiksarkar.3120@gmail.com>
2021-08-17 02:54:48 +0530

    Update script

--- bench-out/bench.sh
+++ bench-out/bench.sh
@@ -3,8 +3,8 @@
 set -e
 
 csvfile="bench-out/bench-out.csv"
-mdfile="bench-out/bench-out.md"
+summaryfile="bench-out/bench-out.txt"
 rm -f "${csvfile}"
 stack build
 stack bench --benchmark-arguments="--csv ${csvfile}"
-stack exec gen-md "${csvfile}" "${mdfile}"
+stack exec gen-md "${csvfile}" "${summaryfile}"
