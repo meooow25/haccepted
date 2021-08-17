05f3308cca8150892ee0f38adf1598cab1107297
meooow25 <soumiksarkar.3120@gmail.com>
2021-08-17 19:46:48 +0530

    Avoid repetition in benchmarks

--- bench/BinSearchBench.hs
+++ bench/BinSearchBench.hs
@@ -11,34 +11,23 @@ import Util
 benchmark :: Benchmark
 benchmark = bgroup "BinSearch" [
         -- n searches on a range of size n
-        bgroup "binSearch" [
-            env (genSearches 100) $ \qs ->
-                bench "100" $ whnf (doSearches qs) 100
-          , env (genSearches 10000) $ \qs ->
-                bench "10000" $ whnf (doSearches qs) 10000
-          , env (genSearches 1000000) $ \qs ->
-                bench "1000000" $ whnf (doSearches qs) 1000000
-        ]
+        bgroup "binSearch" $ map benchBinSeaarch sizes
 
         -- n searches on an array of size n
-      , bgroup "binSearchA" [
-            env (genArrayAndSearches 100) $ \ ~(qs, a) ->
-                bench "100" $ whnf (doSearchesA qs) a
-          , env (genArrayAndSearches 10000) $ \ ~(qs, a) ->
-                bench "10000" $ whnf (doSearchesA qs) a
-          , env (genArrayAndSearches 1000000) $ \ ~(qs, a) ->
-                bench "1000000" $ whnf (doSearchesA qs) a
-        ]
+      , bgroup "binSearchA" $ map benchBinSeaarchA sizes
     ]
 
-genSearches :: Int -> IO [Int]
-genSearches n = return $ randIntsR (1, n) n
+sizes :: [Int]
+sizes = [100, 10000, 1000000]
 
-genArrayAndSearches :: Int -> IO ([Int], Array Int Int)
-genArrayAndSearches n = return (randIntsR (1, n) n, listArray (1, n) [1..n])
+benchBinSeaarch :: Int -> Benchmark
+benchBinSeaarch n = env (gen n) $ \qs -> bench (show n) $ whnf (go qs) n
+    where
+        gen n = return $ randIntsR (1, n) n
+        go qs n = foldl' (\_ i -> binSearch (>= i) 1 n `seq` ()) () qs
 
-doSearches :: [Int] -> Int -> ()
-doSearches qs n = foldl' (\_ i -> binSearch (>= i) 1 n `seq` ()) () qs
-
-doSearchesA :: [Int] -> Array Int Int -> ()
-doSearchesA qs a = foldl' (\_ i -> binSearchA (>= i) a `seq` ()) () qs
+benchBinSeaarchA :: Int -> Benchmark
+benchBinSeaarchA n = env (gen n) $ \ ~(qs, a) -> bench (show n) $ whnf (go qs) a
+    where
+        gen n = return (randIntsR (1, n) n, listArray (1, n) [1..n])
+        go qs a = foldl' (\_ i -> binSearchA (>= i) a `seq` ()) () qs
--- bench/FenwickBench.hs
+++ bench/FenwickBench.hs
@@ -11,44 +11,32 @@ import Util
 benchmark :: Benchmark
 benchmark = bgroup "Fenwick" [
       -- Build a Fenwick tree of size n
-        bgroup "buildF" [
-            bench "100" $ nf genF 100
-          , bench "10000" $ nf genF 10000
-          , bench "1000000" $ nf genF 1000000
-        ]
-      
+        bgroup "buildF" $ map benchBuildF sizes
+
       -- n updates on a Fenwick tree of size n
-      , bgroup "updateF" [
-            env (return (genF 100, genUpds 100)) $ \ ~(ft, us) ->
-                bench "100" $ nf (applyUpds us) ft
-          , env (return (genF 10000, genUpds 10000)) $ \ ~(ft, us) ->
-                bench "10000" $ nf (applyUpds us) ft
-          , env (return (genF 1000000, genUpds 1000000)) $ \ ~(ft, us) ->
-                bench "1000000" $ nf (applyUpds us) ft
-        ]
-      
+      , bgroup "updateF" $ map benchUpdateF sizes
+
       -- n queries on a Fenwick tree of size n
-      , bgroup "queryF" [
-            env (return (genF 100, genQrys 100)) $ \ ~(ft, qs) ->
-                bench "100" $ nf (applyQrys qs) ft
-          , env (return (genF 10000, genQrys 10000)) $ \ ~(ft, qs) ->
-                bench "10000" $ nf (applyQrys qs) ft
-          , env (return (genF 1000000, genQrys 1000000)) $ \ ~(ft, qs) ->
-                bench "1000000" $ nf (applyQrys qs) ft
-        ]
+      , bgroup "queryF" $ map benchQueryF sizes
     ]
+
+sizes :: [Int]
+sizes = [100, 10000, 1000000]
+
+benchBuildF :: Int -> Benchmark
+benchBuildF n = bench (show n) $ nf genF n
+
+benchUpdateF :: Int -> Benchmark
+benchUpdateF n = env (gen n) $ \ ~(ft, us) -> bench (show n) $ nf (go us) ft
+    where
+        gen n = return (genF n, zip (randIntsR (1, n) n) (Sum <$> randInts n))
+        go us ft = foldl' (\ft (i, x) -> updateF i x ft) ft us
+
+benchQueryF :: Int -> Benchmark
+benchQueryF n = env (gen n) $ \ ~(ft, qs) -> bench (show n) $ nf (go qs) ft
+    where
+        gen n = return (genF n, randIntsR (1, n) n)
+        go qs ft = foldl' (\_ i -> queryF i ft `seq` ()) () qs
 
 genF :: Int -> FTree (Sum Int)
 genF n = buildF (1, n)
-
-genUpds :: Int -> [(Int, Sum Int)]
-genUpds n = zip (randIntsR (1, n) n) (Sum <$> randInts n)
-
-genQrys :: Int -> [Int]
-genQrys n = randIntsR (1, n) n
-
-applyUpds :: [(Int, Sum Int)] -> FTree (Sum Int) -> FTree (Sum Int)
-applyUpds upds ft = foldl' (\ft (i, x) -> updateF i x ft) ft upds
-
-applyQrys :: [Int] -> FTree (Sum Int) -> ()
-applyQrys qs ft = foldl' (\_ i -> queryF i ft `seq` ()) () qs
