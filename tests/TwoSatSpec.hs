b4b9053b80df5237ee30eb36f37ccfab292e42ba
meooow25 <soumiksarkar.3120@gmail.com>
2021-09-26 21:54:53 +0530

    Add 2-sat with tests and benchmarks

---
+++ tests/TwoSatSpec.hs
@@ -0,0 +1,46 @@
+module TwoSatSpec where
+
+import Data.Array
+import Data.Maybe
+
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+
+import TwoSat ( Var(..), solve2Sat )
+
+spec :: Spec
+spec = do
+    prop "solve2Sat" $
+        forAll genxys $ \(bnds, xys) -> do
+            let sol = solve2Sat bnds xys
+            label (if isJust sol then "has sol" else "no sol") $
+                sol `shouldSatisfy` validate bnds xys
+
+genxys :: Gen ((Int, Int), [(Var, Var)])
+genxys = sized $ \n' -> do
+    n <- choose (0, n' `min` 15)
+    l <- arbitrary
+    let r = l + n - 1
+        genVar = do
+            b <- arbitrary
+            x <- choose (l, r)
+            pure $ if b then Id x else Not x
+    xys <- if n == 0
+        then pure []
+        else listOf $ (,) <$> genVar <*> genVar
+    pure ((l, r), xys)
+
+validate :: (Int, Int) -> [(Var, Var)] -> Maybe [Bool] -> Bool
+validate bnds xys sol = case sol of
+    Just sol' -> checkSol bnds xys sol'
+    Nothing   -> not $ any (checkSol bnds xys) allOptions
+  where
+    allOptions = mapM (const [False, True]) $ range bnds
+
+checkSol :: (Int, Int) -> [(Var, Var)] -> [Bool] -> Bool
+checkSol bnds xys sol = all (uncurry ok) xys where
+    a = listArray bnds sol
+    get (Id x)  = a!x
+    get (Not x) = not $ a!x
+    ok x y = get x || get y
