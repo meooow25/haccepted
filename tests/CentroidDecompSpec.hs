9797c0982af6e5904e27ba129c06eade07669e45
meooow25 <soumiksarkar.3120@gmail.com>
2021-10-03 15:15:39 +0530

    Return a tree instead of a list

--- tests/CentroidDecompSpec.hs
+++ tests/CentroidDecompSpec.hs
@@ -1,6 +1,5 @@
 module CentroidDecompSpec where
 
-import Control.Monad
 import Data.Foldable
 import Data.List
 import Data.Tree
@@ -17,18 +16,25 @@ spec = do
     prop "centroidDecompose" $
         forAll (snd <$> nonEmpty genTree) $ \t -> do
             let us = sort $ toList t
-                ts = centroidDecompose t
+                rootIsCentroid t = not $ any ((>sz) . (*2)) chSzs where
+                    treeSize = length . toList
+                    sz = treeSize t
+                    chSzs = map treeSize $ subForest t
+                nodeIsMadeUpOfChildren (t, ts) = sort us == sort us' where
+                    us = toList t
+                    us' = rootLabel t : concatMap toList ts
 
-            forM_ ts $ \t -> do
-                -- sanity check, nodes are from the original tree
-                t `shouldSatisfy` (all (`elem` us) . toList)
+                visit (Node t tts) = do
+                    -- sanity check, nodes are from the original tree
+                    t `shouldSatisfy` (all (`elem` us) . toList)
 
-                -- the root is the centroid
-                let rootIsCentroid t = not $ any ((>sz) . (*2)) chSzs where
-                        treeSize = length . toList
-                        sz = treeSize t
-                        chSzs = map treeSize $ subForest t
-                t `shouldSatisfy` rootIsCentroid
+                    -- the root is the centroid in its tree
+                    t `shouldSatisfy` rootIsCentroid
 
-            -- every node only appears exactly once as root
-            sort (map rootLabel ts) `shouldBe` us
+                    -- the tree node in the centroid tree is made up of the nodes in its children
+                    -- trees, plus the root.
+                    (t, map rootLabel tts) `shouldSatisfy` nodeIsMadeUpOfChildren
+
+                    mapM_ visit tts
+
+            visit $ centroidDecompose t
