27adb50cb42ee07bc7782db090e010dc2952c501
meooow25 <soumiksarkar.3120@gmail.com>
2021-10-05 06:26:20 +0530

    Stricter test

--- tests/CentroidDecompSpec.hs
+++ tests/CentroidDecompSpec.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE TupleSections #-}
 module CentroidDecompSpec where
 
 import Data.Foldable
+import Data.Function
 import Data.List
+import Data.Maybe
 import Data.Tree
 
 import Test.Hspec
@@ -20,21 +23,34 @@ spec = do
                     treeSize = length . toList
                     sz = treeSize t
                     chSzs = map treeSize $ subForest t
-                nodeIsMadeUpOfChildren (t, ts) = sort us == sort us' where
-                    us = toList t
-                    us' = rootLabel t : concatMap toList ts
+                nodeIsMadeUpOfChildren t ts = t `treeEq` Node (rootLabel t) ts' where
+                    nodeToSubtree = concatMap (\t -> map (,t) $ toList t) ts
+                    getSubtree v = reroot v $ fromJust $ lookup v nodeToSubtree
+                    ts' = map (getSubtree . rootLabel) $ subForest t
 
-                visit (Node t tts) = do
+                visit (Node t tss) = do
                     -- sanity check, nodes are from the original tree
                     t `shouldSatisfy` (all (`elem` us) . toList)
 
                     -- the root is the centroid in its tree
                     t `shouldSatisfy` rootIsCentroid
 
-                    -- the tree node in the centroid tree is made up of the nodes in its children
-                    -- trees, plus the root.
-                    (t, map rootLabel tts) `shouldSatisfy` nodeIsMadeUpOfChildren
+                    -- the tree node in the centroid tree is made up of its children trees connected
+                    -- by the root
+                    (t, map rootLabel tss) `shouldSatisfy` uncurry nodeIsMadeUpOfChildren
 
-                    mapM_ visit tts
+                    mapM_ visit tss
 
             visit $ centroidDecompose t
+
+treeEq :: Ord a => Tree a -> Tree a -> Bool
+treeEq = (==) `on` sortTree where
+    sortTree (Node u ts) = Node u $ map sortTree $ sortOn rootLabel ts
+
+reroot :: Eq a => a -> Tree a -> Tree a
+reroot r t = fromJust $ go t [] where
+    go (Node u ts) par
+        | u == r    = Just $ Node u $ par ++ ts
+        | otherwise = asum $ zipWith3 f (inits ts) ts (tail $ tails ts)
+      where
+        f left t right = go t [Node u $ par ++ left ++ right]
