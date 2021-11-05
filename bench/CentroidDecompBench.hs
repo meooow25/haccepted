ed15f2555cb544d2998c6bf83ee1eaacc5c95a0c
Soumik Sarkar <soumiksarkar.3120@gmail.com>
2021-11-06 01:17:36 +0530

    Add centroid decomp for labelled trees (#18)
    - Add labelled graph basic stuff
    - Add centroid decomposition for labelled trees
    - Update documentation

--- bench/CentroidDecompBench.hs
+++ bench/CentroidDecompBench.hs
@@ -2,13 +2,16 @@ module CentroidDecompBench where
 
 import Criterion
 
-import CentroidDecomp ( centroidDecompose )
-import Util ( evalR, randTree, sizedBench )
+import CentroidDecomp ( centroidDecompose, centroidDecomposeL )
+import Util ( evalR, randLTree, randTree, sizedBench )
 
 benchmark :: Benchmark
 benchmark = bgroup "CentroidDecomp"
     [ -- Centroid decompose a tree of size n
       bgroup "centroidDecompose" $ map benchCentroidDecompose sizes
+    
+      -- Centroid decompose a labelled tree of size n
+    , bgroup "centroidDecomposeL" $ map benchCentroidDecomposeL sizes
     ]
 
 sizes :: [Int]
@@ -17,3 +20,7 @@ sizes = [100, 10000, 200000]
 benchCentroidDecompose :: Int -> Benchmark
 benchCentroidDecompose n = sizedBench n gen $ nf centroidDecompose where
     gen = evalR $ randTree n
+
+benchCentroidDecomposeL :: Int -> Benchmark
+benchCentroidDecomposeL n = sizedBench n gen $ nf centroidDecomposeL where
+    gen = evalR $ randLTree n
