d4058dba860067b1d5ef6fa85d73861143306073
meooow25 <soumiksarkar.3120@gmail.com>
2022-11-07 22:16:38 +0530

    FenwickBench style improvements

--- bench/FenwickBench.hs
+++ bench/FenwickBench.hs
@@ -28,11 +28,11 @@ benchFromListF n = sizedBench n gen $ nf $ fromListF (1, n) where
     gen = evalR $ map Sum <$> randInts n
 
 benchMappendF :: Int -> Benchmark
-benchMappendF n = sizedBench n gen $ \ ~(ft, us) -> nf (go ft) us where
+benchMappendF n = sizedBench n gen $ \(ft, us) -> nf (go ft) us where
     gen = (emptyF (1, n), evalR $ zip <$> randIntsR (1, n) n <*> (map Sum <$> randInts n))
-    go ft us = foldl' (\ft (i, x) -> mappendF x i ft) ft us
+    go = foldl' (\ft (i, x) -> mappendF x i ft)
 
 benchFoldPrefixF :: Int -> Benchmark
-benchFoldPrefixF n = sizedBench n gen $ \ ~(ft, qs) -> whnf (go ft) qs where
+benchFoldPrefixF n = sizedBench n gen $ \(ft, qs) -> whnf (go ft) qs where
     gen = (emptyF (1, n) :: FTree (Sum Int), evalR $ randIntsR (1, n) n)
-    go ft qs = foldl' (\_ i -> foldPrefixF i ft `seq` ()) () qs
+    go ft = foldl' (\_ i -> foldPrefixF i ft `seq` ()) ()
