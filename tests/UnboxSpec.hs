dc6bccfac8f645a0d76a73b2519c15022ec8f8c6
meooow25 <soumiksarkar.3120@gmail.com>
2023-04-10 19:08:29 +0530

    Split out ArrayTests

--- tests/UnboxSpec.hs
+++ tests/UnboxSpec.hs
@@ -2,51 +2,26 @@
              ScopedTypeVariables, TypeFamilies, TypeApplications #-}
 module UnboxSpec where
 
-import Data.Array.IArray
-import Data.Array.MArray
-
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
 
 import Unbox ( Unbox(..), UArr, IOUArr )
+import ArrayTests ( testListArrayElems, testNewListArrayGetElems, testReadWriteArray )
 
 spec :: Spec
 spec = do
-    describe "UArray" $ do
+    describe "IArray" $ do
         describe "listArray and elems" $ do
-            prop "X Int" $ testListArrayElems @(X Int)
-            prop "Maybe Word" $ testListArrayElems @(Maybe Word)
-    describe "IOUArray" $ do
+            prop "X Int" $ testListArrayElems @UArr @(X Int)
+            prop "Maybe Word" $ testListArrayElems @UArr @(Maybe Word)
+    describe "MArray" $ do
         describe "newListArray and getElems" $ do
-            prop "X Int" $ testNewListArrayGetElems @(X Int)
-            prop "Maybe Word" $ testNewListArrayGetElems @(Maybe Word)
+            prop "X Int" $ testNewListArrayGetElems @IOUArr @(X Int)
+            prop "Maybe Word" $ testNewListArrayGetElems @IOUArr @(Maybe Word)
         describe "writeArray and readArray" $ do
-            prop "X Int" $ testReadWriteArray @(X Int)
-            prop "Maybe Word" $ testReadWriteArray @(Maybe Word)
-  where
-    testListArrayElems :: (Arbitrary a, Eq a, Show a, IArray UArr a) => (Int, [a]) -> Expectation
-    testListArrayElems (l, xs) = do
-        let n = length xs
-            a = listArray @UArr (l, l+n-1) xs
-        elems a `shouldBe` xs
-
-    testNewListArrayGetElems :: (Arbitrary a, Eq a, Show a, MArray IOUArr a IO)
-                             => (Int, [a]) -> Expectation
-    testNewListArrayGetElems (l, xs) = do
-        let n = length xs
-        a <- newListArray @IOUArr (l, l+n-1) xs
-        xs' <- getElems a
-        xs' `shouldBe` xs
-
-    testReadWriteArray :: (Arbitrary a, Eq a, Show a, MArray IOUArr a IO)
-                       => (Positive Int, Int, a) -> Property
-    testReadWriteArray (Positive n, l, x) = do
-        forAll (choose (l, l+n-1)) $ \i -> do
-            a <- newArray_ @IOUArr (l, l+n-1)
-            writeArray a i x
-            x' <- readArray a i
-            x' `shouldBe` x
+            prop "X Int" $ testReadWriteArray @IOUArr @(X Int)
+            prop "Maybe Word" $ testReadWriteArray @IOUArr @(Maybe Word)
 
 newtype X a = X a deriving (Eq, Ord, Show, Arbitrary)
 
